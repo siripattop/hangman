2fd70d7f8042be8d96f7b1bcdd9f269a92ca8505
siripattop <50234205+siripattop@users.noreply.github.com>
2019-05-07 21:59:43 +0700

    Update ServerClientConnection.java

--- ServerClientConnection.java
+++ ServerClientConnection.java
@@ -1,12 +1,5 @@
-
-/**
- * Maintains an abstract connection on the Server side with a certain Client to send and receive data.
- *
- */
 public abstract class ServerClientConnection {
-	/**
-	 * Tracks the number of played games for this client.
-	 */
+	
 	protected int gamesCounter;
 
 	public ServerClientConnection() {
@@ -14,14 +7,7 @@ public abstract class ServerClientConnection {
 	}
 
 	public abstract void dealWithClient();
-
-	/**
-	 * Handles the PLAY/STOP request from the Client.
-	 *
-	 * @param request
-	 *            the command protocol for requesting a new game
-	 * @return <code>true</code> if the Client is now playing and <code>false</code> otherwise
-	 */
+	
 	protected boolean handleRequest(String request) {
 		if (request.equals(Server.GAME_PLAY)) {
 			if (this.gamesCounter == Server.hangmanGames.size()) {
@@ -41,15 +27,6 @@ public abstract class ServerClientConnection {
 		return false;
 	}
 
-	/**
-	 * Checks that the Client input is valid and checks the guess character in the game.
-	 *
-	 * @param game
-	 *            the Hangman game being currently played
-	 * @param input
-	 *            the guess character of the Client
-	 * @return <code>true</code> if the Client entered valid character and <code>false</code> otherwise
-	 */
 	protected boolean checkGuess(Hangman game, String input) {
 		if (input != null && input.length() > 0) {
 			input = input.trim();
